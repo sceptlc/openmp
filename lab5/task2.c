ded60cd668d61e09681cefe66b35c1c3e6691be2
sceptlc <sceptlc@bk.ru>
2020-10-21 20:55:54 +0300

    lab5 task2

--- lab5/task2.c
+++ lab5/task2.c
@@ -1,64 +1,64 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <stdbool.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include <omp.h>
-#include <limits.h> 
+#include <limits.h>
 
-void merge(int arr[], int beg, int mid, int end, int maxele) 
-{ 
-	int i = beg; 
-	int j = mid + 1; 
-	int k = beg; 
-	while (i <= mid && j <= end) { 
-		if (arr[i] % maxele <= arr[j] % maxele) { 
-			arr[k] = arr[k] + (arr[i] % maxele) * maxele; 
-			k++; 
-			i++; 
-		} 
-		else { 
-			arr[k] = arr[k] + (arr[j] % maxele) * maxele; 
-			k++; 
-			j++; 
-		} 
-	} 
-	while (i <= mid) { 
-		arr[k] = arr[k] + (arr[i] % maxele) * maxele; 
-		k++; 
-		i++; 
-	} 
-	while (j <= end) { 
-		arr[k] = arr[k] + (arr[j] % maxele) * maxele; 
-		k++; 
-		j++; 
-	} 
+void merge(int arr[], int start, int mid, int end)
+{
+	int start2 = mid + 1;
+	// If the direct merge is already sorted
+	if (arr[mid] <= arr[start2])
+		return;
 
-	// Obtaining actual values 
-	for (int i = beg; i <= end; i++) 
-		arr[i] = arr[i] / maxele; 
-} 
+	// Two pointers to maintain start
+	// of both arrays to merge
+	while (start <= mid && start2 <= end)
+	{
+		// If element 1 is in right place
+		if (arr[start] <= arr[start2])
+		{
+			start++;
+		}
+		else
+		{
+			int value = arr[start2];
+			int index = start2;
 
-void merge_sort_rec(int arr[], int beg, int end, int maxele) 
-{ 
-	if (beg < end) { 
-		int mid = (beg + end) / 2; 
-		merge_sort_rec(arr, beg, mid, maxele); 
-		merge_sort_rec(arr, mid + 1, end, maxele); 
-		merge(arr, beg, mid, end, maxele); 
-	} 
-} 
+			// Shift all the elements between element 1
+			// element 2, right by 1.
+			while (index != start)
+			{
+				arr[index] = arr[index - 1];
+				index--;
+			}
+			arr[start] = value;
 
-int max_element (int arr[], int n) {
-	int max = INT_MIN;
-	for (int i = 0; i < n; i++)
-		if (arr[i] > max) max = arr[i];
-	return max;
+			// Update all the pointers
+			start++;
+			mid++;
+			start2++;
+		}
+	}
 }
 
-void merge_sort(int arr[], int n, int maxele) 
-{ 
-	merge_sort_rec(arr, 0, n-1, maxele); 
-} 
+/* l is for left index and r is right index of the 
+sub-array of arr to be sorted */
+void merge_sort(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+		// Same as (l + r) / 2, but avoids overflow
+		// for large l and r
+		int m = l + (r - l) / 2;
+
+		// Sort first and second halves
+		merge_sort(arr, l, m);
+		merge_sort(arr, m + 1, r);
+		merge(arr, l, m, r);
+	}
+}
 
 void fill_rand(int *arr, int n)
 {
@@ -70,62 +70,70 @@ void fill_rand(int *arr, int n)
 bool test_sorted(int *arr, int n)
 {
 	for (int i = 1; i < n; i++)
-		if (arr[i] < arr[i - 1]) return false;
+		if (arr[i] < arr[i - 1])
+			return false;
 	return true;
 }
 
-int main() 
-{ 
-	int n = 33; 
-	int arr[n]; 
-
-	fill_rand(arr, n);
-	int maxele = max_element(arr, n) + 1; 
-	int threads_n = 0; // remember number of threads in parallel block
-
-	int sumbef = 0, sumaft = 0;
-	for (int i = 0; i < n; sumbef += arr[i], i++);
-
+void print_arr(int *arr, int n)
+{
 	for (int i = 0; i < n; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
+}
 
-	#pragma omp parallel num_threads(3) shared(arr)
+int main()
+{
+	double start = omp_get_wtime();
+	const int n = 500000;
+	int arr[n];
+	int threads_n = 0;			// remember number of threads in parallel block
+	int sumbef = 0, sumaft = 0; // checksums to compare
+
+	fill_rand(arr, n);
+	for (int i = 0; i < n; sumbef += arr[i], i++)
+		;
+
+/**
+	 * Sort chunks in parallel
+	 */
+#pragma omp parallel num_threads(8) shared(arr)
 	{
 		const int i = omp_get_thread_num();
 		const int thr_n = omp_get_num_threads();
 		const int share = n / thr_n;
-		const int left  = i * share;
+		const int left = i * share;
 		// if this is a last chunk, take all that remains
-		const int right = (i == thr_n - 1) ? n - 1: (i + 1) * share - 1;
-		// printf("thread #%d: indices %d to %d\n", i, left, right);
+		const int right = (i == thr_n - 1) ? n - 1 : (i + 1) * share - 1;
+		printf("thread #%d: indices %d to %d\n", i, left, right);
 
-		#pragma omp single
-			threads_n = thr_n;
+#pragma omp single
+		threads_n = thr_n;
 
-		merge_sort(arr + left, right - left + 1, maxele);
+		merge_sort(arr, left, right);
 	}
 
+	/**
+	 * Merge sorted chunks
+	 */
 	const int share = (n / threads_n);
 	int mid, right;
-	for (int i = 1; i < threads_n; i++) {	
+	for (int i = 1; i < threads_n; i++)
+	{
 		mid = i * share - 1;
 		// if this is a last chunk, take all that remains
-		right = (i == threads_n - 1) ? n - 1: (i + 1) * share - 1;
-		
-		// printf("merging arr[] left=%d, mid=%d, right=%d\n", 0, mid, right);
-		merge(arr, 0, mid, right, maxele);
+		right = (i == threads_n - 1) ? n - 1 : (i + 1) * share - 1;
+
+		printf("merging arr[] left=%d, mid=%d, right=%d\n", 0, mid, right);
+		merge(arr, 0, mid, right);
 	}
 
-	for (int i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-	
-
+	double end = omp_get_wtime();
 
 	sumaft = 0;
-	for (int i = 0; i < n; sumaft += arr[i], i++);
-	
+	for (int i = 0; i < n; sumaft += arr[i], i++)
+		;
+
 	if (test_sorted(arr, n))
 		printf("Test passed!\n");
 	else
@@ -136,7 +144,7 @@ int main()
 	else
 		printf("[!!!] Sum before = %d, after = %d!\n", sumbef, sumaft);
 
-	
+	printf("[Time passed]: %lf\n", end - start);
 
 	return 0;
-} 
+}
